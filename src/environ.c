a67776acbdafea5c49e0e71c174909bb06531d8e
Vilya Harvey <vilya.harvey@gmail.com>
2011-11-11 11:41:19 +0000

    Correctly detect the OS bit size.

--- src/environ.c
+++ src/environ.c
@@ -1,8 +1,10 @@
 #include "environ.h"
 
 #include <assert.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/utsname.h> // for uname()
 
 
 //
@@ -14,18 +16,32 @@ extern char** environ;
 
 
 //
+// Forward declarations
+//
+
+// Determine the bit size of the current operating system. This correctly
+// handles the case where this program has been compiled as a 32-bit app and is
+// running on a 64-bit OS, for example. The return value will be a number like
+// 32 or 64.
+unsigned int fetchdeps_environ_bitsize();
+
+
+//
 // Public functions
 //
 
 bool_t
 fetchdeps_environ_default_vars(varmap_t* vm)
 {
+  char bits[10];
+
   assert(vm != NULL);
 
   if (!fetchdeps_varmap_set_single(vm, "os", kOperatingSystem))
     return 0;
-  // TODO: set the bit size correctly for the current platform.
-  if (!fetchdeps_varmap_set_single(vm, "bits", "64"))
+
+  snprintf(bits, 10, "%u", fetchdeps_environ_bitsize());
+  if (!fetchdeps_varmap_set_single(vm, "bits", bits))
     return 0;
 
   return 1;
@@ -74,5 +90,32 @@ bool_t
 fetchdeps_environ_get_vars(varmap_t* vm)
 {
   return fetchdeps_environ_parse_vars(vm, environ);
+}
+
+
+//
+// Private functions
+//
+
+unsigned int
+fetchdeps_environ_bitsize()
+{
+  struct utsname buf;
+  unsigned int bits;
+
+  // First, try to parse the bit size from the result of a uname() call. This
+  // should correctly handle a 32-bit program running on a 64-bit OS.
+  if (uname(&buf) == 0) {
+    if (strncmp("x86_", buf.machine, 4) == 0)
+      bits = atoi(buf.machine + 4);
+    else if (strcmp("x86", buf.machine) == 0)
+      bits = 32; 
+    return bits;
+  }
+
+  // If that doesn't work, use the bit width of a pointer. This will incorrectly
+  // return 32 if we're a 32-bit program running on a 64-bit OS, but it's the
+  // best we can do at this point.
+  return sizeof(void*) * 8;
 }
 
